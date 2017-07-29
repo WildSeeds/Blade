71940d10d2e6c0f1af4e23b4d630bbbb1258c6d2
WildSeeds <1017577452@qq.com>
2017-07-29 21:00:33 +0800

    增加菜单修改

---
+++ Lib/menu.py
@@ -0,0 +1,107 @@
+﻿#coding=utf-8
+'''
+Created on 2017年7月21日
+
+@author: Administrator
+'''
+
+import Lib.ClassSelenium as ClassSelenium
+import time
+import pickle
+import builtins
+from lib2to3.tests.support import driver
+from warnings import catch_warnings
+global driver
+global InputData
+global UIExcept
+
+class menu(object):
+    def __init__(self,menuwindow,leaf):
+        self.leaf =leaf
+        self.menuself = menuwindow
+        self.menulink = self.getmenulink()
+        self.name =self.getname()
+        self.submenulist =self.submenu()
+    def getmenulink(self):
+        if self.leaf > 0:
+            return self.menuself.getelementbyattribute(r'xpath:a')
+        else:
+            return None
+    def getname(self):
+        if self.leaf > 0:
+            return self.menulink.gettext()
+        else:
+            return "主菜单"
+    def submenu(self):
+        if self.leaf > 0:
+            try:
+                menuelelist = self.menuself.getelementbyattribute(r'xpath:ul/li',getall = True)
+            except Exception:
+                return None
+        else:
+            try:
+                menuelelist =  self.menuself.getelementbyattribute(r'xpath:li',getall = True)
+            except Exception:
+                return None
+        return menuelelist
+    def openallmenu(self):
+        for i in self.submenulist:
+            j = menu(i,1)
+            j.getmenulink().Click();
+            #time.sleep(1)
+            if j.submenulist:
+                for k in j.submenulist:
+                    m = menu(k,2)
+                    m.getmenulink().Click();
+                    #time.sleep(1)
+                    if m.submenulist:
+                        for n in m.submenulist :
+                            p = menu(n,3)
+                            p.getmenulink().Click();
+                            #time.sleep(1)
+
+            
+            
+            
+class menunavigation(object):
+    def __init__(self,menunavigation):
+        self.menunavigationself = menunavigation
+        self.forward = menunavigation.getelementbyattribute('xpath:div[1]')
+        self.label=menunavigation.getelementbyattribute('xpath:div[2]')
+        self.backward = menunavigation.getelementbyattribute('xpath:div[3]')
+        self.zoom = menunavigation.getelementbyattribute('xpath:div[4]')
+        self.View_toolbar = menunavigation.getelementbyattribute('xpath:div[5]')
+    def getmenutable(self,name): 
+        tabellist = self.label.getelementbyattribute('tag name:li',getall = True) #获取标签页按钮
+        for i in tabellist:
+            tabelspan = i.getelementbyattribute('tag name:span',getall = True)
+            if tabelspan[0].gettext() == name:
+                if len(tabelspan) > 1:
+                    return (tabelspan[0],tabelspan[1])
+                else:
+                    return (tabelspan[0],None)
+        return None
+            
+        
+        
+        
+
+def init():
+    global driver
+    driver=ClassSelenium.ClassSelenium("http://10.20.27.63:8080/am/login.htm,chrome")
+    driver.getelementbyattribute("id:vc_op_code").sendkeys('8888')
+    driver.getelementbyattribute('id:vc_op_password').sendkeys("123456")   #��
+    driver.getelementbyattribute('id:login').Click()
+    menubody = driver.getelementbyattribute(r'css selector:.h-menu-body>ul')
+    navigation = driver.getelementbyattribute(r'css selector:.h-screen-tab')
+    temtable = menunavigation(navigation)
+    a = temtable.getmenutable('主页')
+    a[0].Click()
+    print(a[0].gettext())
+    fbmenu = menu(menubody,0) 
+    fbmenu.openallmenu()
+   
+if __name__ == '__main__':
+    init()
+    
+    pass
--- Lib/stockaccount.py
+++ Lib/stockaccount.py
@@ -9,13 +9,15 @@ import Lib.ClassSelenium as ClassSelenium
 import time
 import pickle
 from lib2to3.tests.support import driver
+from warnings import catch_warnings
 global driver
 global InputData
 global UIExcept
 
+
 def init():
     global driver
-    driver=ClassSelenium.ClassSelenium("http://10.20.27.63:8080/am/login.htm,chrome")
+    driver=ClassSelenium.ClassSelenium("http://192.168.70.237:8080/am/login.htm,chrome")
     driver.getelementbyattribute("id:vc_op_code").sendkeys('8888')
     driver.getelementbyattribute('id:vc_op_password').sendkeys("123456")   #��
     driver.getelementbyattribute('id:login').Click()
@@ -28,12 +30,18 @@ def init():
     #获取用户功能菜单
     global buttonmenu
     buttonmenu=driver.getelementbyattribute("xpath:.//*[@id='wrap_dataTable']/div[1]")
+
+
 def end():
     time.sleep(15)
     driver.close()
+
+
 def clicktable(num=0):
     operatablearray=operatable.getelementbyattribute('tag name:tr',True) #查找所有的行
     operatablearray[num].Click()
+
+
 def addOp():
     grid9=buttonmenu.getelementbyattribute('link text:新增')
     grid9.Click()
@@ -44,18 +52,47 @@ def addOp():
     addwin.getelementbyattribute('tag name:input,name:vc_op_name').sendkeys("webui")
     addwin.getelementbyattribute('tag name:input,name:vc_bm_name').sendkeys("webui")
     addwin.getelementbyattribute('tag name:button,text:取消').Click()
+
+
 def modifyOp():
     clicktable(0)
     grid9=buttonmenu.getelementbyattribute('link text:修改')
     grid9.Click()
     addwin=driver.getelementbyattribute("id:editWin")
-    a=addwin.getelementbyattribute('tag name:input,ref:ztree_vc_branch_id_edit')
+    a = addwin.getelementbyattribute(r'xpath:descendant::label[@title="部门编号"]/following-sibling::*/descendant::*/input[last()]')
     a.Click()
     driver.getelementbyattribute("id:ztree_vc_branch_id_edit_1_span").Click()
-    addwin.getelementbyattribute('tag name:input,name:vc_op_name').sendkeys("webui")
-    addwin.getelementbyattribute('tag name:input,name:vc_bm_name').sendkeys("webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="用户名称"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="用户别名"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="电话号码"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="手机号码"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="联系地址"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="电子邮件"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="身份证号"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    addwin.getelementbyattribute(
+        r'xpath:descendant::label[@title="其他信息"]/following-sibling::*/descendant::*/input[last()]').sendkeys(
+        "webui")
+    time.sleep(10)
     addwin.getelementbyattribute('tag name:button,text:取消').Click()
     clicktable(0)
+
+
+
+
 def passRest():
     clicktable(0)
     grid9=buttonmenu.getelementbyattribute('link text:密码重置')
@@ -63,6 +100,8 @@ def passRest():
     addwin=driver.getelementbyattribute("id:h_msg_floatdiv")
     addwin.getelementbyattribute('tag name:button,text:取消').Click()
     clicktable(0)
+
+
 def passCancel():
     clicktable(0)
     grid9=buttonmenu.getelementbyattribute('link text:注销')
@@ -70,6 +109,8 @@ def passCancel():
     addwin=driver.getelementbyattribute("id:logoutWin")
     addwin.getelementbyattribute('tag name:button,text:取消').Click()
     clicktable(0)
+
+
 def roleApply():
     clicktable(0)
     grid9=buttonmenu.getelementbyattribute('link text:角色分配')
@@ -77,42 +118,44 @@ def roleApply():
     addwin=driver.getelementbyattribute("id:logoutWin")
     addwin.getelementbyattribute('tag name:button,text:取消').Click()
     clicktable(0)
+
+def exceptioncheck(windriver):
+    try:
+        windriver.getelementbyattribute(r'css selector:.verify-tip-inner',getall = True)
+    except Exception as ex:
+        pass
+    else:
+        raise ClassSelenium.SeleniumError("css.verify-tip-inner:"+"输入值有误")
+
+
 def login():
-    global driver
-    driver=ClassSelenium.ClassSelenium("http://10.20.27.63:8080/am/login.htm,chrome")
-    driver.getelementbyattribute("id:vc_op_code").sendkeys('8888')
-    driver.getelementbyattribute('id:vc_op_password').sendkeys("123456")
-    driver.getelementbyattribute('id:login').Click()
-    driver.getelementbyattribute('link text:系统管理').Click()#菜单
-    driver.getelementbyattribute('link text:系统配置管理').Click()#菜单
-    driver.getelementbyattribute('link text:用户管理').Click()#菜单
-    global operatable
-    operatable=driver.getelementbyattribute('tag name:table,id:data_table_dataTable')
-    #获取用户功能菜单
-    global buttonmenu
-    buttonmenu=driver.getelementbyattribute("xpath:.//*[@id='wrap_dataTable']/div[1]")
     clicktable(0)
     grid9=buttonmenu.getelementbyattribute('link text:修改')
     grid9.Click()
-    addwin=driver.getelementbyattribute("id:editWin")
+    addwin=driver.getelementbyattribute(r'xpath:/html/body/child::div[@style="display: block;"]/div[2]')
     #/html/body/descendant::label[@title="部门编号"]/following-sibling::*/descendant::*/input[last()]
     a=addwin.getelementbyattribute(r'xpath:descendant::label[@title="部门编号"]/following-sibling::*/descendant::*/input[last()]')
     a.Click()
     driver.getelementbyattribute("id:ztree_vc_branch_id_edit_1_span").Click()
     #addwin.getelementbyattribute(r'xpath:descendant::label[@title="部门编号"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    addwin.getelementbyattribute(r'xpath:/html/body/descendant::label[@title="部门编号"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
+    addwin.getelementbyattribute(r'xpath:descendant::label[@title="用户名称"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
     addwin.getelementbyattribute(r'xpath:descendant::label[@title="用户别名"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    addwin.getelementbyattribute(r'xpath:descendant::label[@title="电话号码"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    addwin.getelementbyattribute(r'xpath:descendant::label[@title="手机号码"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    addwin.getelementbyattribute(r'xpath:descendant::label[@title="联系地址"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    addwin.getelementbyattribute(r'xpath:descendant::label[@title="电子邮件"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    addwin.getelementbyattribute(r'xpath:descendant::label[@title="身份证号"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    addwin.getelementbyattribute(r'xpath:descendant::label[@title="其他信息"]/following-sibling::*/descendant::*/input[last()]').sendkeys("webui")
-    time.sleep(10)
-    addwin.getelementbyattribute('tag name:button,text:取消').Click()
+
+    exceptioncheck(addwin)
+    addwin.getelementbyattribute('tag name:button,text:确定').Click()
+    confirmwin=driver.getelementbyattribute(r"xpath:.//*[@id='h_msg_floatdiv']") #获取弹出的窗体
+    confirmwin.getelementbyattribute(r"xpath:.//div[2]/div/div,text:请您确认是否提交？")
+    confirmwin.getelementbyattribute('tag name:button,text:确定').Click()
+    #addwin.getelementbyattribute('tag name:button,text:取消').Click()
+    htip = driver.getelementbyattribute(r"xpath://div[@class='h_tips']")
+    htip.getelementbyattribute('tag name:div,text:操作成功！')
+    print("操作成功")
     clicktable(0)
+    
+    time.sleep(10)
     #driver.close()
     #return 'Pass',''
 if __name__ == '__main__':
+    init()
     login()
     end()
